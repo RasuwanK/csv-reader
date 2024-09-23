bcd43ca88dddacfe7c40e1d16be9e86b56f334c5
Rasuwan Kalhara <kalharaweragala@gmail.com>
2024-09-23 15:50:20 +0530

    Added size constraints

--- include/sql_parse.h
+++ include/sql_parse.h
@@ -2,6 +2,9 @@
 #define SQL_PARSE_H
 
 #define TOKEN_SIZE 256
+#define MAX_COLS 100
+#define TABLE_NAME_LEN 100
+#define MAX_COND 100
 
 /* List of tokens which are allowed */
 typedef enum {
@@ -24,6 +27,7 @@ typedef enum {
 } TokenType;
 
 typedef enum {
+  INVALID_COND,
   EQUAL_COND,
   LT_COND, /* LT refers to less than */
   GT_COND, /* GT refers to greater than */
@@ -37,30 +41,30 @@ typedef struct {
   char value[TOKEN_SIZE];
 } Token;
 
-/* Shape of a select query */
-typedef struct {
-  /* Querying table name */
-  char* table_name;
-  /* Number of columns to be queried */
-  int num_columns;
-  /* Holding all the column names */
-  char (*columns)[TOKEN_SIZE];
-  /* condition for WHERE */
-  Condition condition;
-} SelectQuery;
-
 /* Structing the way to interpret a condition */
 typedef struct {
   char identifier[TOKEN_SIZE];
   ConditionType type;
-  char value;
+  char value[TOKEN_SIZE];
 } Condition;
 
-SelectQuery build_select_query(char* sql);
-Token next_token(const char** sql);
+/* Shape of a select query */
+typedef struct {
+  /* Querying table name */
+  char table_name[TABLE_NAME_LEN];
+  /* Number of columns to be queried */
+  int num_columns;
+  /* Holding all the column names */
+  char columns[MAX_COLS][TOKEN_SIZE];
+  /* condition for WHERE */
+  /* Now only support for a single condition TODO: Support
+  for multiple conditions */
+  Condition* conditions;
+  /* For reporting errors */
+  int invalid;
+} SelectQuery;
 
-SelectQuery build_select_query(char* sql) {
-  SelectQuery query;
-}
+SelectQuery build_select_query(const char* sql);
+Token next_token(const char** sql);
 
 #endif
